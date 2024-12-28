50732a5a01dc8ef81b2bc656cb09db9b28425809
LexiCodes <lexicodes@lexicodes.net>
2024-12-29 03:06:09 +0400

    Initial Commit

---
+++ src/client/java/net/lexicodes/culinaryquest/client/CulinaryquestClient.java
@@ -0,0 +1,10 @@
+package net.lexicodes.culinaryquest.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CulinaryquestClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
