75557b807f81963c4a2d52453acafa05818277e9
LexiCodes <lexicodes@lexicodes.net>
2024-12-29 15:34:16 +0400

    Changed to using Fabric template mod

---
+++ src/client/java/net/lexicodes/CulinaryQuestClient.java
@@ -0,0 +1,10 @@
+package net.lexicodes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CulinaryQuestClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
